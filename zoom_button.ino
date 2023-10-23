a68576eec09c4d21cb78c375fa90d7f854df50c8
Olga Pavlova <olgapavlova@gmail.com>
2023-10-23 20:25:21 +0300

    Preferences for Mac Zoom

--- zoom_button.ino
+++ zoom_button.ino
@@ -21,14 +21,14 @@ void loop() {
 
     if (btn.click()) {
       Serial.println("Hand");
-      Keyboard.click(KEY_LEFT_SHIFT, KEY_Y);
+      Keyboard.click(KEY_LEFT_ALT, KEY_1); // Alt + 1 — поднять/опустить руку (нужно перенастроить Zoom)
       clicked = true;
       clickedMoment = millis();
     }
 
-    if ((clicked) && (millis() - clickedMoment > 3000)) {
+    if ((clicked) && (millis() - clickedMoment > 3000)) { // Три секунды
       Serial.println("Mic");
-      Keyboard.click(KEY_LEFT_SHIFT, KEY_A);      
+      Keyboard.click(KEY_LEFT_SHIFT, KEY_LEFT_ALT, KEY_0); // Shift + Alt + 0 — включить/выключить микрофон (нужно перенастроить Zoom)     
       clicked = false;
     }
 
