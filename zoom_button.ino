f9af926c4e702e6ba62f71482007b520b4d95607
Olga Pavlova <olgapavlova@gmail.com>
2023-10-24 16:06:49 +0300

    Working version, 1 button

--- zoom_button.ino
+++ zoom_button.ino
@@ -21,14 +21,14 @@ void loop() {
 
     if (btn.click()) {
       Serial.println("Hand");
-      Keyboard.click(KEY_LEFT_ALT, KEY_1); // Alt + 1 — поднять/опустить руку (нужно перенастроить Zoom)
+      Keyboard.click(KEY_LEFT_ALT, KEY_1); // Alt + 1
       clicked = true;
       clickedMoment = millis();
     }
 
-    if ((clicked) && (millis() - clickedMoment > 3000)) { // Три секунды
+    if ((clicked) && (millis() - clickedMoment > 3000)) { // 3 sec
       Serial.println("Mic");
-      Keyboard.click(KEY_LEFT_SHIFT, KEY_LEFT_ALT, KEY_0); // Shift + Alt + 0 — включить/выключить микрофон (нужно перенастроить Zoom)     
+      Keyboard.click(KEY_LEFT_SHIFT, KEY_LEFT_ALT, KEY_0); // Shift + Alt + 0
       clicked = false;
     }
 
