7d16b8cc006e35d18e46f7b72cbe7d6ac7b22a76
Torill Thorstad Hauger <olgapavlova@gmail.com>
2023-11-03 15:03:16 +0300

    APM + 3 buttons

--- zoom_button.ino
+++ zoom_button.ino
@@ -1,15 +1,22 @@
 #include <Keyboard.h>
 
-int pinButton = 8;
-int pinLED = 2;
+int pinButtonRed = 7;
+int pinButtonYellow = 8;
+int pinButtonBlack = A3;
+int pinLED = 9;
 
-bool flag = false;
+bool flagRed = false;
+bool flagYellow = false;
+bool flagBlack = false;
 
 void click_hand_on_off();
 void click_sound_on_off();
+void click_chat_on_off();
 
 void setup() {
-  pinMode(pinButton, INPUT);
+  pinMode(pinButtonRed, INPUT);
+  pinMode(pinButtonYellow, INPUT);
+  pinMode(pinButtonBlack, INPUT);
   pinMode(pinLED, OUTPUT);
   digitalWrite(pinLED, HIGH);
 }
@@ -17,20 +24,40 @@ void setup() {
 
 void loop() {
   // читаем инвертированное значение для удобства
-  bool btnState = !digitalRead(pinButton);
-  if (btnState && !flag) {  // обработчик нажатия
-    flag = true;
+  bool btnStateRed = !digitalRead(pinButtonRed);
+  bool btnStateYellow = !digitalRead(pinButtonYellow);
+  bool btnStateBlack = !digitalRead(pinButtonBlack);
+
+  if (btnStateRed && !flagRed) {  // обработчик нажатия красной кнопки
+    flagRed = true;
     digitalWrite(pinLED, HIGH);
-    click_hand_on_off();
     click_sound_on_off();
-    
   }
-  if (!btnState && flag) {  // обработчик отпускания
-    flag = false;  
+
+  if (btnStateYellow && !flagYellow) {  // обработчик нажатия жёлтой кнопки
+    flagYellow = true;
+    digitalWrite(pinLED, HIGH);
+    click_hand_on_off();    
+  }
+
+  if (btnStateBlack && !flagBlack) {  // обработчик нажатия чёрной кнопки
+    flagBlack = true;
+    digitalWrite(pinLED, HIGH);
+    click_chat_on_off();    
+  }
+
+  if (!btnStateRed && flagRed) {  // обработчик отпускания
+    flagRed = false;  
     digitalWrite(pinLED, LOW);
-    Keyboard.press(KEY_LEFT_SHIFT);
-    Keyboard.write('b');
-    Keyboard.release(KEY_LEFT_SHIFT);
+  }
+
+  if (!btnStateYellow && flagYellow) {  // обработчик отпускания
+    flagYellow = false;  
+    digitalWrite(pinLED, LOW);
+
+  if (!btnStateBlack && flagBlack) {  // обработчик отпускания
+    flagBlack = false;  
+    digitalWrite(pinLED, LOW);
   }
 }
 
@@ -44,5 +71,12 @@ void click_sound_on_off() {
   Keyboard.press(KEY_LEFT_ALT);
   Keyboard.press(KEY_LEFT_SHIFT);
   Keyboard.write('0');
+  Keyboard.releaseAll();
+}
+
+void click_chat_on_off() {
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.write('4');
   Keyboard.releaseAll();
 }
