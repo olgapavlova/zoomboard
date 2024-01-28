42eb9c35093b3949596d3f59de7bd74d71b28d64
olgapavlova <olgapavlova@gmail.com>
2024-01-28 23:57:35 +0300

    nano versions

--- playground_digispark_one_button.ino
+++ playground_digispark_one_button.ino
@@ -1,13 +1,34 @@
 /*
 
-  Эксперимент с подключением одной кнопки к Digispark.
-  Измеряем, сколько нужно памяти.
+  Прошивка для трёхкнопочной клавиатуры.
+
+  Кнопки отправляют команды в Zoom:
+  -- поднять/опустить руку;
+  -- включить/выключить звук;
+  -- показать/скрыть чат.
+
+  PIN_HEAD  Пин кнопки поднятия/опускания руки.
+  PIN_SOUND Пин кнопки вкл/выкл звука.
+  PIN_CHAT  Пин кнопки отображения чата.
+
+  DEBUG Если переменная определена, клавиатура переходит в отладочный режим
+        -- кнопки отправляют только чистые символы, без клавиш-модификаторов.
 
 */
 
-#include <DigiKeyboard.h>
+#include <Keyboard.h>
 
-#define PIN_BUTTON  0
+#define DEBUG 1
+
+#define PIN_HAND  7
+#define PIN_SOUND 8
+#define PIN_CHAT  9
+
+#ifdef DEBUG
+#define KEYBOARD_PRESS(x)
+#else
+#define KEYBOARD_PRESS(x) Keyboard.press(x)
+#endif
 
 // Класс работы с кнопкой
 class ZoomButton {
@@ -54,11 +75,15 @@ class ZoomButton {
 
 };
 
-// функция-обработчик кнопки
-void click_button_on_off();
+// функции-обработчики для разных кнопок
+void click_hand_on_off();
+void click_sound_on_off();
+void click_chat_on_off();
 
 // кнопки с привязкой к Zoom-функциональности
-ZoomButton buttonButton(PIN_BUTTON, click_hand_on_off);
+ZoomButton handButton(PIN_HAND, click_hand_on_off);
+ZoomButton soundButton(PIN_SOUND, click_sound_on_off);
+ZoomButton chatButton(PIN_CHAT, click_chat_on_off);
 
 /*
   СТАРТ ПРОГРАММЫ
@@ -75,7 +100,9 @@ void setup() {
   ОСНОВНОЙ ЦИКЛ ПРОГРАММЫ
 */
 void loop() {
-  buttonButton.check_and_react();
+  handButton.check_and_react();
+  soundButton.check_and_react();
+  chatButton.check_and_react();
 }
 
 /*
@@ -85,7 +112,24 @@ void loop() {
 // TODO шаблонизировать функции нажатия горячих клавиш
 
 // поднять-опустить руку
-void click_button_on_off() {
-  DigiKeyboard.write('1');
-  DigiKeyboard.releaseAll();
+void click_hand_on_off() {
+  KEYBOARD_PRESS(KEY_LEFT_ALT);
+  Keyboard.write('1');
+  Keyboard.releaseAll();
+}
+
+// вкл-выкл звук
+void click_sound_on_off() {
+  KEYBOARD_PRESS(KEY_LEFT_ALT);
+  KEYBOARD_PRESS(KEY_LEFT_SHIFT);
+  Keyboard.write('0');
+  Keyboard.releaseAll();
+}
+
+// показать-скрыть чат
+void click_chat_on_off() {
+  KEYBOARD_PRESS(KEY_LEFT_ALT);
+  KEYBOARD_PRESS(KEY_LEFT_SHIFT);
+  Keyboard.write('4');
+  Keyboard.releaseAll();
 }
